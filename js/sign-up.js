c126923a63b5029104caca4f4121645b70133ca8
Samed Shukurov <samedzauroglu2007@gmail.com>
2023-04-25 12:20:20 +0400

    fixed bug

--- js/sign-up.js
+++ js/sign-up.js
@@ -1,7 +1,9 @@
-
+const agree = document.querySelector(".agree");
 const eyeButton = document.querySelector(".fa-eye");
 const password = document.querySelector("#password") ;
-
+agree.addEventListener("click",()=>{
+    agree.classList.toggle("agreed");
+})
 eyeButton.addEventListener("click",()=>{  
     if(eyeButton.classList.contains("fa-eye")){
         eyeButton.classList.replace("fa-eye","fa-eye-slash")
