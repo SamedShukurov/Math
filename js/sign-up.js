950cc9d26319e81aa9e01f9525a582ac0a4b642c
Samed Shukurov <samedzauroglu2007@gmail.com>
2023-04-30 16:58:58 +0400

    fixed bug in sign up

--- js/sign-up.js
+++ js/sign-up.js
@@ -1,8 +1,18 @@
 const agree = document.querySelector(".agree");
 const eyeButton = document.querySelector(".fa-eye");
 const password = document.querySelector("#password") ;
+const student = document.querySelector(".first");
+const teacher = document.querySelector(".second");
 agree.addEventListener("click",()=>{
     agree.classList.toggle("agreed");
+})
+student.addEventListener("click",()=>{
+    student.classList.toggle("agreed");
+    teacher.classList.remove("agreed");
+})
+teacher.addEventListener("click",()=>{
+    teacher.classList.toggle("agreed");
+    student.classList.remove("agreed");
 })
 eyeButton.addEventListener("click",()=>{  
     if(eyeButton.classList.contains("fa-eye")){
